07133e3302b1b6762699763f0f5d7df4a7f82b1d
witchwildterror <238302543+witchwildterror@users.noreply.github.com>
2026-05-16 17:29:41 +0300

    add flake.nix + flake.lock

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          gdb
+          fltk_1_4
+          sqlite
+          pkg-config
+        ];
+      };
+    };
+}
